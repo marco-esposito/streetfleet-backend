349eb70af919d95589def15a5d0d7b445754a98e
dromomaniac <evanlhendrix@gmail.com>
2018-03-19 17:47:05 +0100

    connect database with mongoose

--- app.js
+++ app.js
@@ -7,13 +7,18 @@ const app = new Koa();
 
 const logger = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
+const cors = require('kcors');
 
 const config = require('./config');
 const router = require('./router');
 
+// requiring the db like this is executing the code right here.
+require('./db');
+
 /**
  * Middlewhere
  */
+app.use(cors());
 app.use(logger());
 app.use(bodyParser());
 
@@ -21,12 +26,8 @@ app.use(bodyParser());
  * Routes
  */
 app.use(router.routes());
-// app.use(router.allowedMethods());
+app.use(router.allowedMethods());
 
-
-/**
- * 404
- */
 
 app.listen(config.port).on('error', err => {
   console.error(err);
