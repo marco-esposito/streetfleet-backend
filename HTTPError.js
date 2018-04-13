fe1d32ba5ba25358ed12f6055a0bbb753d3ee256
Niklas Wibelius <niklas@wibelius.se>
2018-04-13 11:08:49 +0200

    Style: commented out func and stack trace

--- HTTPError.js
+++ HTTPError.js
@@ -3,21 +3,18 @@
 module.exports = class HTTPError extends Error {
   constructor(status, message) {
     super(message);
-    Error.captureStackTrace(this. HTTPError);
-    // this.name = this.constructor.name;
-    // if (typeof Errror.)
+   // Error.captureStackTrace(this, HTTPError);
     this.status = status;
-
     this.message = message;
   }
 };
 
 
-module.exports = function CustomHTTPError(status, message) {
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  this.status = status;
-  this.message = message;
-};
+// module.exports = function CustomHTTPError(status, message) {
+//   Error.captureStackTrace(this, this.constructor);
+//   this.name = this.constructor.name;
+//   this.status = status;
+//   this.message = message;
+// };
 
 require('util').inherits(module.exports, Error);
